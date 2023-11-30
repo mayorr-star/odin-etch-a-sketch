f1d685e7a06eda84b120a36ffe5e375ec649a5fd
mayorr-star <immaboat11@gmail.com>
2023-11-30 17:49:37 +0000

    Create a new grid when i click the button

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
 const grid = document.querySelector(".grid");
+const gridcontainer = document.querySelector(".grid-container");
 
-function createGrid() {
-    for (let i = 0; i < 16 * 16; i++) {
+window.addEventListener("DOMContentLoaded", createGrid(16));
+
+function createGrid(size) {
+    for (let i = 0; i < size * size; i++) {
             const squareDiv = document.createElement("div");
             squareDiv.setAttribute("class", "square-div");
             grid.appendChild(squareDiv);
             squareDiv.addEventListener("mouseover", (e) => displayColour(e))
     }
 }
-createGrid()
 
 function displayColour(e) {
-    e.currentTarget.classList.add("hover-me")
+    e.currentTarget.classList.add("hover-me");
 }
+
+function createButton() {
+    const button = document.createElement("button");
+    button.setAttribute("class", "size-button")
+    const buttonTextNode = document.createTextNode("Change size");
+    button.appendChild(buttonTextNode)
+    gridcontainer.insertBefore(button, grid)
+
+    button.addEventListener("click", () => {
+        const newSize = Number(prompt("Enter new size"));
+        grid.innerHTML = "";
+        createGrid(newSize)
+    })
+}
+createButton()
+
