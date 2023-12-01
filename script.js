1c18b2bb9c5a667b9335ca0c8b01c39cc4d5675a
mayorr-star <immaboat11@gmail.com>
2023-12-01 21:35:18 +0000

    Creted a function to change grid size based on users input

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const grid = document.querySelector(".grid");
 const buttonBox = document.querySelector(".button-box");
+const buttons = document.querySelectorAll(".extra");
 
 window.addEventListener("DOMContentLoaded", createGrid(16))
 
 function createGrid(size) {
+    grid.innerHTML = ""
     for (let rows = 0; rows < size; rows++) {
         const row = document.createElement("div");
         row.setAttribute("class", "row");
@@ -22,3 +24,19 @@ function createGrid(size) {
 function changeColour(e) {
     e.currentTarget.classList.add("cell-hover")
 }
+
+function changeGridSize(newSize) {
+    createGrid(newSize);
+}
+
+buttons.forEach(button => {
+    button.addEventListener("click", (e) => {
+        const buttonId = button.getAttribute("id");
+        switch (buttonId) {
+            case "change-size":
+                const newSize = Number(prompt("Enter size of grid"));
+                changeGridSize(newSize);
+                break;
+        }
+    })
+})
